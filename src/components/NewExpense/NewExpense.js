2d23bd864847dcf22927b21006655a39c74870f0
Ravi Chaudhary <malhotraravi842@gmail.com>
2021-04-17 12:41:57 +0530

    completed module-4

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,21 @@
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (data) => {
+    const expenseData = {
+      id: Math.random().toString(),
+      ...data,
+    };
+
+    props.newExpenseData(expenseData);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
